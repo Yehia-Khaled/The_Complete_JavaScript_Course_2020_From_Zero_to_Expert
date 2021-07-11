f0fe9c1c97e202d1cbd5393d95cc43ca01c302f3
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-07-11 19:05:16 +0200

    code lecture 032 Activating Strict Mode

--- 03 JavaScript Fundamentals – Part 2/02-Fundamentals-Part-2/starter/script.js
+++ 03 JavaScript Fundamentals – Part 2/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+let hasDriversLicense =false;
+const passTest =true;
+
+if(passTest) hasDriversLicense =true;
+if (hasDriversLicense) console.log('i can drive :D');
+
+//const interface ='audio';
+//const private =258;  reserved words get error on console when use strict mode
+
