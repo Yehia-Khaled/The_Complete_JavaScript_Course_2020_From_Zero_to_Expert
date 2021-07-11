00e8647b772b2bca05d6563f1d9f97360e8c3790
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-07-11 19:50:01 +0200

    refactor and move file

---
+++ 03 JavaScript Fundamentals – Part 2/script.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+Lecture 032 Activating Strict Mode
+let hasDriversLicense =false;
+const passTest =true;
+
+if(passTest) hasDriversLicense =true;
+if (hasDriversLicense) console.log('i can drive :D');
+
+//const interface ='audio';
+//const private =258;  reserved words get error on console when use strict mode
+*/
+/*033 Functions*/
+// function simply a piesce of code can use over and over again in our code.
+
+function logger(){
+    console.log("my name is yehia")
+}
+// calling /running /invoking function
+logger();
+logger();
+logger();
+logger();
