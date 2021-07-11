de802fee05f2971fb95c1ed79dc79bd4b96be32c
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-07-11 20:30:38 +0200

    lecture 033 Functions code

--- 03 JavaScript Fundamentals – Part 2/Assignments.js
+++ 03 JavaScript Fundamentals – Part 2/Assignments.js
@@ -5,7 +5,7 @@
 function returns a string with this format: 'Finland has 6 million people and its
 capital city is Helsinki'
 2. Call this function 3 times, with input data for 3 different countries. Store the
-returned values in 3 different variables, and log them to the console*/
+returned values in 3 different variables, and log them to the console
 //require 1 create function
 function describeCountry(country,population,capitalCity){
     return(`${country} has ${population} million people, and its capital city is ${capitalCity}`)
@@ -16,3 +16,4 @@ const finlandDescribtion=describeCountry("Finland",6,"Helsinki");
 const americaDescribtion=describeCountry("America",331," Washington");
 // print result on console
 console.log(egyptDescribtion,"\n",finlandDescribtion,"\n",americaDescribtion);
+*/
--- 03 JavaScript Fundamentals – Part 2/script.js
+++ 03 JavaScript Fundamentals – Part 2/script.js
@@ -10,7 +10,9 @@ if (hasDriversLicense) console.log('i can drive :D');
 //const interface ='audio';
 //const private =258;  reserved words get error on console when use strict mode
 */
-/*033 Functions*/
+/*
+//033 Functions
+
 // function simply a piesce of code can use over and over again in our code.
 
 function logger(){
@@ -20,4 +22,4 @@ function logger(){
 logger();
 logger();
 logger();
-logger();
+logger();*/
