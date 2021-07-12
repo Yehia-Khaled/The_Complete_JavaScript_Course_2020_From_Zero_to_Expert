2058fe2025c8e51db2456b3e986f88dec1fec301
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-07-12 18:02:51 +0200

    lecture 035 Arrow Functions code

--- 03 JavaScript Fundamentals – Part 2/script.js
+++ 03 JavaScript Fundamentals – Part 2/script.js
@@ -24,6 +24,7 @@ logger();
 logger();
 logger();
 */
+/*
 //034 Function Declarations vs. Expressions
 //function declarations
 const age1=calaAge1(1999);
@@ -39,15 +40,31 @@ function calaAge1 (birthyear){
 const calcAge2= function (birthyear){
     return 2037- birthyear;
 }
-/*function are actually just values ' function not a type ' not like a string or number type,
- so as it is a value we can store them in a variables*/
+/!*function are actually just values ' function not a type ' not like a string or number type,
+ so as it is a value we can store them in a variables*!/
 const age2= calcAge2(1999);
 console.log(age1,age2)
 
-/*
+/!*
 big differences between function declarations and expression
 function declarations : we can actually call function declarations before they are defined in the code"hoisting"
-*/
+*!/
 // can use function expression :
 // - nice structure 'define all function first at the top of the code and only then i can call them '
 // - every thing store in variables
+*/
+
+// 035 Arrow Functions
+// special form of function expression that is shorter and therefore faster to write
+
+// function expression /anonymous function 'function without a name'
+const calcAge2= function (birthYear){
+    return 2037- birthYear;
+}
+
+// Arrow function
+const calcAge3 =birthYear => 2037-birthYear;
+const age3=calcAge3(1999)
+console.log(age3)
+
+//for now wa actually keep using mainly the normal functions , except for one -linear functions.
