8bc2dc5fe116beef8522bcf1f9e07c47ea33c126
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-07-12 17:45:13 +0200

    lecture 034 Function Declarations vs. Expressions code

--- 03 JavaScript Fundamentals – Part 2/script.js
+++ 03 JavaScript Fundamentals – Part 2/script.js
@@ -22,4 +22,32 @@ function logger(){
 logger();
 logger();
 logger();
-logger();*/
+logger();
+*/
+//034 Function Declarations vs. Expressions
+//function declarations
+const age1=calaAge1(1999);
+
+function calaAge1 (birthyear){
+    return 2037- birthyear;
+}
+
+// console.log(age1);
+
+// function expression /anonymous function 'function without a name'
+// expression produce a value so we assign a whole value to this variable calcAge2
+const calcAge2= function (birthyear){
+    return 2037- birthyear;
+}
+/*function are actually just values ' function not a type ' not like a string or number type,
+ so as it is a value we can store them in a variables*/
+const age2= calcAge2(1999);
+console.log(age1,age2)
+
+/*
+big differences between function declarations and expression
+function declarations : we can actually call function declarations before they are defined in the code"hoisting"
+*/
+// can use function expression :
+// - nice structure 'define all function first at the top of the code and only then i can call them '
+// - every thing store in variables
