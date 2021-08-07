98d209686ebc46c949ab7f9fc5e8e7b42e652ed7
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-08-08 00:55:12 +0200

    lecture 055 Setting up Prettier and VS Code my written code,and notes.

--- 05 Developer Skills & Editor Setup/script.js
+++ 05 Developer Skills & Editor Setup/script.js
@@ -1,2 +1,21 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+/* 055 Setting up Prettier and VS Code */
+//prettier :is an opinionated code formater ,which means that  it makes assumptions about how good code should look like, make your code is nicely formatted .
+/*
+// used extentions
+1-Prettier - Code formatter
+2-Auto close tag
+3-Auto rename tag
+4-ESLint
+5-image preview 
+6-monokai pro
+7-setting sync 
+8-TODO Highlight
+*/
+
+const x = 23;
+
+const calcAge = birtheyear => console.log(2037 - birtheyear);
+calcAge(1999);
+console.log();
