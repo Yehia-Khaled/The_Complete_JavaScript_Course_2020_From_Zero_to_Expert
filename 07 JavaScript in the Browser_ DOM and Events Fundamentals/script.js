0354bbe05a8648da5cb477662990c05167625c41
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-08-24 23:04:49 +0200

    lecture  071 What's the DOM and DOM Manipulation my written code,and notes.

--- 07 JavaScript in the Browser_ DOM and Events Fundamentals/script.js
+++ 07 JavaScript in the Browser_ DOM and Events Fundamentals/script.js
@@ -1,3 +1,13 @@
 'use strict';
+/*
 //070 PROJECT #1_ Guess My Number!
-console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.message').textContent);*/
+
+//071 What's the DOM and DOM Manipulation
+
+//DOM Methods and properties for DOM manipulation NOT PART OF JS
+// if the DOM is not a part of the javaScript language ,then how does this all work?
+//the DOM an the DOM methods are actually part of something called the web APIs.
+// Web APIs: are like libraries that browser implement  and that we can access from our JavaScript code.
+//API stands for Application Programming Interface.
+//Web APIs are basically libraries that are also written in javaScript and that are ,automatically available for us to use.
